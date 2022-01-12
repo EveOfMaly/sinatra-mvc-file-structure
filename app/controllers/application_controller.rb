04b79f8b02188f1d58f901a41ac7f5586c90dcf3
EveOfMaly <john.a.kyeremeh@gmail.com>
2022-01-12 11:02:12 -0500

    Completed assignment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,14 @@
 class ApplicationController < Sinatra::Base
 
+
+  #this block tells the controller where to look to find the views and public library 
   configure do
   	set :views, "app/views"
   	set :public_dir, "public"
   end
 
+
+  #controller action - actions to accept an respond to html
   get "/" do
   	erb :index
   end
--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 ENV['SINATRA_ENV'] ||= "development"
 ENV['RACK_ENV'] ||= "development"
 
-require 'bundler/setup'
+require 'bundler/setup' #loads bundler and all gems in gemfil.
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
 require_all 'app'
